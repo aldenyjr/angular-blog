0f2db4e77259f5bdd8efa96c7f8a89acb706b103
Aldeny Junior <aldenyjr.dev@gmail.com>
2023-09-04 14:34:24 -0300

    components dinamycs propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent {
-
+  @Input()
+  photoCover: string =
+    'https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png';
+  @Input()
+  cardTitle: string = 'Texto Padrão';
 }
